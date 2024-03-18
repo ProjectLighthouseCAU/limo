f7a77b5b8bb0293ff16f1b75a0612affd65c70e7
fwcd <fwcd@fwcd.dev>
2024-03-18 21:36:41 +0100

    Only show current level in ls

--- src/cmd/ls.rs
+++ src/cmd/ls.rs
@@ -4,6 +4,7 @@ use crate::context::Context;
 
 pub async fn invoke(_args: &str, ctx: &mut Context) -> Result<()> {
     let response = ctx.lh.list(&ctx.cwd.as_relative().as_str_vec()).await?;
-    println!("{}", response.payload);
+    let entries: Vec<_> = response.payload.entries.into_keys().collect();
+    println!("{}", entries.join("   "));
     Ok(())
 }
