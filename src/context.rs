49886bbbe4097c0f7d7caef7e14129c2f2dfc776
fwcd <fwcd@fwcd.dev>
2024-03-18 22:18:25 +0100

    Try implementing completion with rustyline
    This currently doesn't work, unfortunately, since the completer is not
    async and invoking block_on panics due to apparently being on the same
    thread.

--- src/context.rs
+++ src/context.rs
@@ -1,15 +1,35 @@
+use std::cell::RefCell;
+
 use lighthouse_client::{Lighthouse, TokioWebSocket};
 use rustyline::{completion::Completer, highlight::Highlighter, hint::Hinter, validate::Validator, Helper};
+use tokio::runtime::Handle;
 
 use crate::path::VirtualPathBuf;
 
 pub struct Context {
-    pub lh: Lighthouse<TokioWebSocket>,
+    // TODO: If/when complete is &mut self, we can remove interior mutability again.
+    pub lh: RefCell<Lighthouse<TokioWebSocket>>,
     pub cwd: VirtualPathBuf,
+    pub handle: Handle,
 }
 
 impl Completer for Context {
     type Candidate = String;
+
+    fn complete(
+        &self,
+        line: &str,
+        _pos: usize,
+        _ctx: &rustyline::Context<'_>,
+    ) -> rustyline::Result<(usize, Vec<Self::Candidate>)> {
+        let child = self.cwd.join(VirtualPathBuf::from(line));
+        let candidates = self.handle.block_on(async {
+            self.lh.borrow_mut().list(&child.as_relative().as_str_vec()).await
+        }).map(|response| {
+            response.payload.entries.into_keys().collect()
+        }).unwrap_or_default();
+        Ok((0, candidates))
+    }
 }
 
 impl Hinter for Context {
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ mod cmd;
 mod context;
 mod path;
 
+use std::cell::RefCell;
+
 use anyhow::Result;
 use clap::Parser;
 use context::Context;
 use lighthouse_client::{protocol::Authentication, Lighthouse, LIGHTHOUSE_URL};
 use path::VirtualPathBuf;
 use rustyline::{config::Configurer, error::ReadlineError, history::DefaultHistory, Editor};
+use tokio::runtime::Handle;
 use url::Url;
 
 #[derive(Parser)]
@@ -33,11 +36,13 @@ async fn main() -> Result<()> {
     let url = Url::parse(&args.url)?;
     let host = url.host_str().unwrap_or("?");
 
+    let handle = Handle::current();
     let mut rl = Editor::<Context, DefaultHistory>::new().unwrap();
     rl.set_auto_add_history(true);
     rl.set_helper(Some(Context {
-        lh: Lighthouse::connect_with_tokio_to(&args.url, auth).await?,
+        lh: RefCell::new(Lighthouse::connect_with_tokio_to(&args.url, auth).await?),
         cwd: VirtualPathBuf::root(),
+        handle,
     }));
 
     loop {
