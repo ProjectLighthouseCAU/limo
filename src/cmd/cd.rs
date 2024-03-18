49392297ef6124441f6b6203ac8022f310f16651
fwcd <fwcd@fwcd.dev>
2024-03-18 22:41:56 +0100

    Check if directory exists before cding

--- src/cmd/cd.rs
+++ src/cmd/cd.rs
@@ -3,6 +3,8 @@ use anyhow::Result;
 use crate::{context::Context, path::VirtualPathBuf};
 
 pub async fn invoke(args: &str, ctx: &mut Context) -> Result<()> {
-    ctx.cwd.push(VirtualPathBuf::from(args));
+    let new_cwd = ctx.cwd.join(VirtualPathBuf::from(args));
+    ctx.lh.list(&new_cwd.as_lh_vec()).await?;
+    ctx.cwd = new_cwd;
     Ok(())
 }
