db51832430dc081484e99be39ec0ceebdbc6874a
fwcd <fwcd@fwcd.dev>
2024-03-18 22:52:57 +0100

    Add TODO to rm command

--- src/cmd/rm.rs
+++ src/cmd/rm.rs
@@ -2,6 +2,9 @@ use anyhow::Result;
 
 use crate::{context::Context, path::VirtualPathBuf};
 
+// TODO: Add a failsafe to only delete individual resources and not directories
+// We could support the classic -rf flags for that.
+
 pub async fn invoke(args: &str, ctx: &mut Context) -> Result<()> {
     let path = ctx.cwd.join(VirtualPathBuf::from(args));
     ctx.lh.delete(&path.as_lh_vec()).await?;
