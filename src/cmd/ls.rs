d5ac25e38eb8fbec83b5cd2fa28e0b3af438ba5d
fwcd <fwcd@fwcd.dev>
2024-03-19 00:22:04 +0100

    Support argument in ls

--- src/cmd/ls.rs
+++ src/cmd/ls.rs
@@ -1,9 +1,10 @@
 use anyhow::Result;
 
-use crate::context::Context;
+use crate::{context::Context, path::VirtualPathBuf};
 
-pub async fn invoke(_args: &str, ctx: &mut Context) -> Result<()> {
-    let response = ctx.lh.list(&ctx.cwd.as_lh_vec()).await?;
+pub async fn invoke(args: &str, ctx: &mut Context) -> Result<()> {
+    let path = ctx.cwd.join(VirtualPathBuf::from(args));
+    let response = ctx.lh.list(&path.as_lh_vec()).await?;
     let entries: Vec<_> = response.payload.entries.into_keys().collect();
     println!("{}", entries.join("   "));
     Ok(())
