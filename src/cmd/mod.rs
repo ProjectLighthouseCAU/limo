5a498c8bacf24268eb4a6c8ece1c4ca5fdd41539
fwcd <fwcd@fwcd.dev>
2024-03-20 18:04:47 +0100

    Add help command

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -9,6 +9,7 @@ macro_rules! cmd_mods {
         async fn interpret(args: &[&str], ctx: &mut Context) -> Result<()> {
             match args[0] {
                 $(stringify!($mod) => $mod::invoke(args, ctx).await?,)*
+                "help" => println!("Available commands: {}", [$(stringify!($mod),)*].join(", ")),
                 cmd => bail!("Unrecognized command: {}", cmd),
             }
             Ok(())
