3bc77b921f30589fbdc5be6cd45b93c7fa0ec160
fwcd <fwcd@fwcd.dev>
2024-03-20 05:10:01 +0100

    Add -p flag to mkdir

--- src/cmd/mkdir.rs
+++ src/cmd/mkdir.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{bail, Result};
 use clap::Parser;
 
 use crate::{context::Context, path::VirtualPathBuf};
@@ -6,6 +6,9 @@ use crate::{context::Context, path::VirtualPathBuf};
 #[derive(Parser)]
 #[command(bin_name = "mkdir")]
 struct Args {
+    #[arg(short, long, help = "Create intermediate directories and don't error if the directory exists")]
+    parents: bool,
+
     #[arg(default_value = ".", help = "The directory to create")]
     path: VirtualPathBuf,
 }
@@ -13,6 +16,16 @@ struct Args {
 pub async fn invoke(args: &[&str], ctx: &mut Context) -> Result<()> {
     let args = Args::try_parse_from(args)?;
     let path = ctx.cwd.join(args.path);
-    ctx.lh.mkdir(&path.as_lh_vec()).await?;
+    if path.is_root() {
+        bail!("Cannot create root directory");
+    }
+    let parent_exists = ctx.lh.list(&path.parent().as_lh_vec()).await.is_ok();
+    if !args.parents && !parent_exists {
+        bail!("{} does not exist, pass -p to create intermediate directories", path.parent())
+    }
+    let result = ctx.lh.mkdir(&path.as_lh_vec()).await;
+    if !args.parents {
+        result?;
+    }
     Ok(())
 }
