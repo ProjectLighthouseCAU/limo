1087b1df1c2ed1489bd76f05b30f9e76173d4627
fwcd <fwcd@fwcd.dev>
2024-03-20 04:04:56 +0100

    Add -l flag to ls

--- src/cmd/ls.rs
+++ src/cmd/ls.rs
@@ -6,6 +6,9 @@ use crate::{context::Context, path::VirtualPathBuf};
 #[derive(Parser)]
 #[command(bin_name = "ls")]
 struct Args {
+    #[arg(short, long, action, help = "Use a long listing format")]
+    long: bool,
+
     #[arg(default_value = ".")]
     path: VirtualPathBuf,
 }
@@ -15,6 +18,13 @@ pub async fn invoke(args: &[&str], ctx: &mut Context) -> Result<()> {
     let path = ctx.cwd.join(args.path);
     let response = ctx.lh.list(&path.as_lh_vec()).await?;
     let entries: Vec<_> = response.payload.entries.into_keys().collect();
-    println!("{}", entries.join("   "));
+    if args.long {
+        // TODO: Print more metadata
+        for entry in entries {
+            println!("{}", entry);
+        }
+    } else {
+        println!("{}", entries.join("   "));
+    }
     Ok(())
 }
