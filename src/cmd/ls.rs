309ec4d8db7cbc157433b24f6c96c82d770e4f38
fwcd <fwcd@fwcd.dev>
2024-03-20 04:08:28 +0100

    Add -a flag to ls

--- src/cmd/ls.rs
+++ src/cmd/ls.rs
@@ -6,6 +6,9 @@ use crate::{context::Context, path::VirtualPathBuf};
 #[derive(Parser)]
 #[command(bin_name = "ls")]
 struct Args {
+    #[arg(short, long, action, help = "Include . and ..")]
+    all: bool,
+
     #[arg(short, long, action, help = "Use a long listing format")]
     long: bool,
 
@@ -17,7 +20,10 @@ pub async fn invoke(args: &[&str], ctx: &mut Context) -> Result<()> {
     let args = Args::try_parse_from(args)?;
     let path = ctx.cwd.join(args.path);
     let response = ctx.lh.list(&path.as_lh_vec()).await?;
-    let entries: Vec<_> = response.payload.entries.into_keys().collect();
+    let mut entries: Vec<_> = response.payload.entries.into_keys().collect();
+    if args.all {
+        entries.extend([".", ".."].map(|s| s.to_string()));
+    }
     if args.long {
         // TODO: Print more metadata
         for entry in entries {
