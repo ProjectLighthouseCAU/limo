b9b5598e5862aa327faa4ef2cc4177c8946bafee
fwcd <fwcd@fwcd.dev>
2024-03-21 01:10:42 +0100

    Catch empty line in interpreter

--- src/line/interpret.rs
+++ src/line/interpret.rs
@@ -1,10 +1,13 @@
-use anyhow::Result;
+use anyhow::{bail, Result};
 
 use crate::{cmd, context::Context};
 
 use super::parse::CommandLine;
 
 pub async fn interpret(line: CommandLine, ctx: &mut Context) -> Result<()> {
+    if line.args.is_empty() {
+        bail!("Cannot interpret empty line");
+    }
     let output = cmd::invoke(&line.args, ctx).await?;
     if !output.is_empty() {
         print!("{}", output);
